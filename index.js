16383c0a95229331f89c37312c0a812b110cb338
mgalindosrl <100387001+mgalindosrl@users.noreply.github.com>
2022-11-22 13:23:27 -0600

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get('/api/conversaciones/usuario/:id', (req, res) => {
             res.status(200).json(response);
         })
         .catch((error)=>{
-            console.log(*******************ERROR NUEVA API*********************);
+            console.log("*******************ERROR NUEVA API*********************");
             res.sendStatus(403);
         })
 })
